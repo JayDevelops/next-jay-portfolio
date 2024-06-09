441fbb65d49b614089d9b0085da41590721dc1ab
Jay.develops <jaymations1234@gmail.com>
2024-06-08 22:01:13 -0700

    refactored AboutMeHeader to separate file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next"
 import Link from "next/link"
 import {Button} from "@/components/ui/button"
-import {DownloadIcon} from "@radix-ui/react-icons";
+import {DownloadIcon} from "@radix-ui/react-icons"
+import {AboutMeHeader} from "@/app/about/AboutMeHeader"
 
 export const metadata: Metadata = {
     title: "Work",
@@ -16,40 +17,6 @@ export default function AboutPage() {
     )
 }
 
-export function AboutMeHeader() {
-    return (
-        <article className="prose dark:prose-invert">
-            <h1>
-                Hello my name is <span className="text-primary">Jesus Perez</span> but
-                everyone calls me <span className="text-primary">Jay.</span>
-            </h1>
-
-            <p>
-                My parents come from Michoacan, Mexico (not Michigan) came to Southern California in the
-                1990&apos;s, where I currently
-                reside in Montebello, California. I bring extensive expertise in crafting dynamic web applications
-                using cutting-edge technologies
-                such as Next.js, ReactJS, PHP, ASP.NET, Wordpress and various more in my four plus years of being a
-                FullStack Developer.
-            </p>
-
-            <p>
-                Back in my high school days I was enrolled in a&nbsp;<Link href="https://www.mhscats.com/">creative
-                arts pathway program</Link> dedicated to animation and graphic design, from there I fell in love with
-                design where I wanted to create web design. My animation teacher set me aside showing
-                me his personal portfolio with some fancy jQuery animations.
-            </p>
-
-            <p>
-                Peaking my curiosity into JavaScript land where I wrote my first line of JavaScript code, fast forward to today
-                and the long and continuing path of pursuing my Bachelors in Computer Science and various roles to being a FullStack
-                Developer widens,&nbsp;<Link href="/contact">contact me</Link> to bring me aboard to assist the shipping of your next
-                product.
-            </p>
-            <DownloadResumeButton />
-        </article>
-    )
-}
 
 export function DownloadResumeButton() {
     return (
