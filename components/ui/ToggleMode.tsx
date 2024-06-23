2c784c267c0a2b6eff5fc219f8db1e1b40d752e9
Jay.develops <jaymations1234@gmail.com>
2024-06-22 22:45:37 -0700

    updated dark mode theme toggle between two states only

--- components/ui/ToggleMode.tsx
+++ components/ui/ToggleMode.tsx
@@ -5,36 +5,31 @@ import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export default function ModeToggle() {
-    const { setTheme } = useTheme()
+    const { theme, setTheme, systemTheme } = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
 
+    //  initially sets default theme to system, otherwise on click will toggle between light and dark mode
+    const toggleTheme = () => {
+        if (currentTheme === "dark") {
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+    }
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button
+            variant="outline"
+            size="icon"
+            onClick={toggleTheme}
+        >
+            {currentTheme === "light" ? (
+                <SunIcon className="h-[1.2rem] w-[1.2rem] transition-all duration-100" />
+            ) : (
+                <MoonIcon className="h-[1.2rem] w-[1.2rem] transition-all duration-100" />
+            )}
+            <span className="sr-only">Toggle theme</span>
+        </Button>
     )
 }
