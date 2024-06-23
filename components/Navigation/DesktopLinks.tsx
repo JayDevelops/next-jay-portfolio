ccae6ea9dd21287d6683d4ce135f5c5f1a024be0
Jay.develops <jaymations1234@gmail.com>
2024-06-22 22:21:09 -0700

    updated navigation menu to be nested in <ul>

--- components/Navigation/DesktopLinks.tsx
+++ components/Navigation/DesktopLinks.tsx
@@ -2,7 +2,7 @@ import {usePathname} from "next/navigation";
 import {
     NavigationMenu,
     NavigationMenuItem,
-    NavigationMenuLink,
+    NavigationMenuLink, NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import {menuLinks} from "@/components/Navigation/MenuLinks";
 import Link from "next/link";
@@ -14,22 +14,24 @@ export function DesktopLinks() {
 
     return (
         <NavigationMenu className="ml-9 flex items-center gap-2 text-sm lg:gap-4 list-none space-x-6">
-            {menuLinks.map((item, index) => (
-                <NavigationMenuItem key={`menu-link-${index}`} className="text-secondary-foreground hover:text-secondary-foreground">
-                    <Link href={item.path} legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={cn({
-                                    'text-secondary-foreground': item.path === currentPath,
-                                    'text-muted-foreground': item.path !== currentPath,
-                                    'hover:text-secondary-foreground transition-colors text-sm': true,
-                                }
-                            )}
-                        >
-                            {item.title}
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-            ))}
+            <NavigationMenuList className="space-x-10">
+                {menuLinks.map((item, index) => (
+                    <NavigationMenuItem key={`menu-link-${index}`} className="text-secondary-foreground hover:text-secondary-foreground">
+                        <Link href={item.path} legacyBehavior passHref>
+                            <NavigationMenuLink
+                                className={cn({
+                                        'text-secondary-foreground': item.path === currentPath,
+                                        'text-muted-foreground': item.path !== currentPath,
+                                        'hover:text-secondary-foreground transition-colors text-sm': true,
+                                    }
+                                )}
+                            >
+                                {item.title}
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                ))}
+            </NavigationMenuList>
         </NavigationMenu>
     )
 }
