3ab20b7c5555206b6d89f52fc5c6b35c331b72d0
Jay.develops <jaymations1234@gmail.com>
2024-12-14 18:01:39 -0800

    Updated og.png

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,24 @@ import HomeHeader from "@/app/HomeHeader";
 import dynamic from "next/dynamic";
 
 export default function Home() {
-    const DynamicComputer = dynamic(() => import("@/components/Models/Computer/Computer"), {
-        loading: () => <p>Loading</p>
-    })
-    
-    return (
-        <div className="flex items-center justify-center md:h-screen">
-          <section id="header" className="grid md:grid-cols-2 md:gap-4 mx-4 md:m-auto">
-              <div className="h-[400px] md:w-full mx-auto">
-                <DynamicComputer />
-              </div>
-              <HomeHeader />
-          </section>
-      </div>
-  )
+  const DynamicComputer = dynamic(
+    () => import("@/components/Models/Computer/Computer"),
+    {
+      loading: () => <p>Loading</p>,
+    }
+  );
+
+  return (
+    <div className="flex items-center justify-center md:h-screen">
+      <section
+        id="header"
+        className="grid md:grid-cols-2 md:gap-4 mx-4 md:m-auto"
+      >
+        <div className="h-[400px] md:w-full mx-auto">
+          <DynamicComputer />
+        </div>
+        <HomeHeader />
+      </section>
+    </div>
+  );
 }
