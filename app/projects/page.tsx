349314262e80e35737b074c453d769cd27682086
Jay.develops <jaymations1234@gmail.com>
2024-12-27 18:50:24 -0800

    Added New Life project and updated issue tracker project.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,7 @@ export default function ProjectsPage() {
             </HeadingOne>
             <Text className="my-2 mb-8">
                 I bring extensive expertise in crafting dynamic web applications using cutting-edge technologies such as Next.js, ReactJS, PHP, and WordPress.
-                My proficiency extends to working with advanced tools and databases, including Prisma, React Query, MySQL, and MongoDB.
+                My proficiency extends on working with advanced tools and databases, including Prisma, React Query, MySQL, and MongoDB.
                 My primary focus is on utilizing Next.js in conjunction with ReactJS to develop aesthetically pleasing and highly
                 functional web solutions. Check out some featured projects below, and{' '}
                 <Link href="/contact" className="underline text-primary hover:outline-primary">contact me</Link> for your next project.
--- app/projects/projectsLinks.ts
+++ app/projects/projectsLinks.ts
@@ -1,26 +1,30 @@
 export interface ProjectLink {
-    title: string,
-    githubLink?: string,
-    liveLink?: string,
+  title: string;
+  githubLink?: string;
+  liveLink?: string;
 }
 
 export const projectLinks: ProjectLink[] = [
-    {
-        title: "Issue Tracker",
-        githubLink: "https://github.com/JayDevelops/2024-issue-tracker",
-        liveLink: "https://issuetracker.jesusperez.dev/",
-    },
-    {
-        title: "Sound Pillars",
-        githubLink: "https://github.com/JayDevelops/sound-pillars",
-        liveLink: "https://soundpillars.jesusperez.dev/",
-    },
-    {
-        title: "University Times",
-        liveLink: "https://csulauniversitytimes.com/",
-    },
-    {
-        title: "Swift Pong Game",
-        githubLink: "https://github.com/JayDevelops/pongGame-master",
-    },
-]
+  {
+    title: "Issue Tracker",
+    githubLink: "https://github.com/JayDevelops/2024-issue-tracker",
+    liveLink: "https://issuetracker.jesusperez.dev/",
+  },
+  {
+    title: "Sound Pillars",
+    githubLink: "https://github.com/JayDevelops/sound-pillars",
+    liveLink: "https://soundpillars.jesusperez.dev/",
+  },
+  {
+    title: "University Times",
+    liveLink: "https://csulauniversitytimes.com/",
+  },
+  {
+    title: "Swift Pong Game",
+    githubLink: "https://github.com/JayDevelops/pongGame-master",
+  },
+  {
+    title: "New Life Medical Site",
+    liveLink: "https://newlifeadhc.org/",
+  },
+];
