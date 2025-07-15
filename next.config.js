22c2896af630adf30142cbbc48a3240d553f3331
jay.develops <swejesusperez@gmail.com>
2025-07-14 20:11:13 -0700

    removed proxy in next config js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const { withPlausibleProxy } = require("next-plausible");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // your Next.js configuration options here
 };
 
-module.exports = withPlausibleProxy()(nextConfig);
+module.exports = nextConfig;
